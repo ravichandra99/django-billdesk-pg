ba37e2b8c5cd87e8561b60ec6a96a6e28ffe65de
Your Name <you@example.com>
2021-09-28 12:49:51 +0530

    static

--- payments/views.py
+++ payments/views.py
@@ -6,9 +6,11 @@ from payments.django_billdesk import ResponseMessage, GetMessage
 import uuid
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-from transfer.models import Profile
 from django.utils import timezone
 import hashlib
+from django.contrib.auth.decorators import login_required
+# from django.core.mail import send_mail
+from django.utils.text import slugify
 
 # Create your views here.
 #function to get unique order_id
@@ -16,19 +18,19 @@ def get_order_id(id):
     return str(id)+str(uuid.uuid4())[:8]
 
 #function to generate message and send to billdesk
+@login_required
 def payment_request(request):
     if request.method == 'POST':
         form = checkoutForm(request.POST)
         if form.is_valid():
             amount = 0
-            email_id = form.cleaned_data['email']
-            usr_details = User.objects.filter(id = request.user.id)
-            profile = Profile.objects.get(user = request.user)
-            if usr_details:
+            email_id = slugify(form.cleaned_data['email'])
+            if User.objects.filter(id = request.user.id).exists():
+                usr_details = User.objects.filter(id = request.user.id)
                 usr_details = usr_details[0]
-                fname = usr_details.username
+                fname = slugify(usr_details.username)
                 id = usr_details.pk
-                mnumber = profile.mobile  #assuming u have a another table called User with mob_number as a field
+                mnumber = '9885098850'  #assuming u have a another table called User with mob_number as a field
                 while True:
                     oid = get_order_id(id) 
                     #we use a combo of user's id and uuid to generate a unique order_id
@@ -37,15 +39,33 @@ def payment_request(request):
                     trans = Transaction.objects.filter(order_id=oid)
                     if not trans:
                         break
-                amount = Amount.objects.all().first().amount
+                if Amount.objects.all().first().exists():
+                    amount = Amount.objects.all().first().amount
+                else:
+                    amount = 10.00
                 msg = GetMessage().message(oid, amount, id, email_id, fname, mnumber)
-                print(msg)
                 Transaction.objects.create(owner=usr_details, order_id=oid, email=usr_details.email, amount_initiated=amount, status='PENDING', registered_for='transfer', log=str([msg]), txn_date=timezone.localtime(timezone.now()))
                 return render(request, 'paymentProcess.html', {'msg': msg, 'url': settings.BILL_URL})
             else:
-            #print('not found')
-                error = "Given Email ID doesn't exist."
-                return render(request, 'paymentspage.html', {'error': error, 'form': form})
+                id = 12345
+                mnumber = '9885098850'
+                fname = 'sampleuser'
+                while True:
+                    oid = get_order_id(id) 
+                    #we use a combo of user's id and uuid to generate a unique order_id
+                    #we use this in a loop and check the genrated the order_i with existing ones in the db
+                    #to make sure its unique
+                    trans = Transaction.objects.filter(order_id=oid)
+                    if not trans:
+                        break
+                if Amount.objects.all().first().exists():
+                    amount = Amount.objects.all().first().amount
+                else:
+                    amount = 10.00
+                msg = GetMessage().message(oid, amount, id, email_id, fname, mnumber)
+                Transaction.objects.create(owner=usr_details, order_id=oid, email=usr_details.email, amount_initiated=amount, status='PENDING', registered_for='transfer', log=str([msg]), txn_date=timezone.localtime(timezone.now()))
+                return render(request, 'paymentProcess.html', {'msg': msg, 'url': settings.BILL_URL})
+
     form = checkoutForm()
     return render(request, 'paymentspage.html', {'form': form})
 
@@ -60,12 +80,20 @@ def handleResponse(request):
         if values and values['MID']==settings.MID:
             transac = Transaction.objects.filter(order_id=values['OrderID'])[0]
             tstat,amnt,txnid,dnt,mode = values['TStat'],values['AMNT'], values['TaxnNo'],values['DnT'],values['TMode']
+            email = values['Email']
             if tstat == '0300' and transac.amount_initiated==float(amnt):
                     id = transac.owner.id
                     reg_for = str(transac.registered_for)
                     usr_details = User.objects.filter(id=id)[0]
                     typ = 'success'
                     msgs = ['Success','Payment Succesful', reg_for]
+                    # send_mail(
+                    #          'DTEAPENGG TRANSFER PAYMENT',
+                    #          'Your payment of Rs.{} was successful'.format(amnt),
+                    #          settings.DEFAULT_FROM_EMAIL,
+                    #          [email],
+                    #          fail_silently=False,
+                    #          )
             elif tstat == '0300' and transac.amount_initiated!=amnt:
                 reg_for = str(transac.registered_for)
                 #transac.status = 'AMOUNT Tampered'
@@ -77,6 +105,7 @@ def handleResponse(request):
                 msgs = ['Failed', 'Billdesk is waiting for the trasaction status from your bank. Will update you as soon as we have any response',reg_for]
                 typ = 'info'
             elif tstat != '0300':
+                reg_for = str(transac.registered_for)
                 if tstat == '0399':
                     detail = 'Invalid Authentication at Bank'
                 elif tstat == 'NA':
@@ -85,7 +114,7 @@ def handleResponse(request):
                     detail = 'error at billdesk'
                 else:
                     detail = 'Payment Failed'
-                    reg_for = str(transac.registered_for)
+                    
                 msgs = ['Failed', detail, reg_for]
                 typ = 'danger'
                 transac.log += str([response])
